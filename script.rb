f5787eceb7d6783ccb1030b0c18aa3cac1d3d9c8
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-03-04 13:48:10 -0800

    Generate all substrings, and update dictionary
    Should've split this into 2 commits.
    
    But first, write function all_substrings_of_word in order to get
    all the substrings of a word. This makes the comparison easier
    since you then just iterate over all of those substrings.
    
    Then for each of those substrings, if it is in the dictionary,
    update the output hash.
    
    End result is a hash relating each found substring to the number
    of times it was found.

--- script.rb
+++ script.rb
@@ -1,5 +1,5 @@
 # lowercase all words, and remove all punctuation except for 
-# apostrophes
+# apostrophes.
 def clean_words(words)
   output = Array.new()
 
@@ -11,11 +11,40 @@ def clean_words(words)
   output
 end
 
-def substrings(string, dictionary)
-  all_words = string.split(' ')
-  
-  cleaned_words = clean_words(all_words)
+def all_substrings_of_word(word)
+  output = Array.new()
 
+  (0...word.length).each do |i|
+    (i...word.length).each do |j|
+      output << word[i..j]
+    end
+  end
+
+  output
 end
 
-puts(substrings("hi, there! How're you doing!", []))
+def substrings(string, dictionary)
+  all_words = string.split(' ')
+  cleaned_words = clean_words(all_words)
+
+  output = Hash.new
+  cleaned_words.each do |word|
+    substrings = all_substrings_of_word(word)
+
+    substrings.each do |substring|
+      if dictionary.include?(substring)
+        current_count = output.dig(substring)
+        unless count # meaning, if count=nil in this case
+          current_count = 0
+        end
+        output[substring] = current_count + 1
+      end
+    end
+  end
+
+  output
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts(substrings("below", dictionary))
+puts(substrings("Howdy partner, sit down! How's it going?", dictionary))
